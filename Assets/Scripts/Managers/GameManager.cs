c822a6f34456dd788a2e64571049a337074a490c
Monczak <szymon.jedrzejczak.sj@gmail.com>
2020-03-31 17:02:40 +0200

    Add selection box and some more tiles

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
+    public Camera mainCamera;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        if (Instance != this) Destroy(gameObject);
+
+        mainCamera = Camera.main;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/UI/SelectionBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionBox : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = GameManager.Instance.mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(Mathf.Round(mousePos.x - .5f) + .5f, Mathf.Round(mousePos.y - .5f) + .5f, -1);
+    }
+}
